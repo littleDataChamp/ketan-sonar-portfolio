31e8f7a2301504644f28b5cda6e30db2256c1cda
Ketan Sonar <ketansonar0708@gmail.com>
2026-08-12 10:51:40 +0530

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/ketan-sonar-portfolio",
+  assetPrefix: "/ketan-sonar-portfolio/",
   images: {
     unoptimized: true,
   },
