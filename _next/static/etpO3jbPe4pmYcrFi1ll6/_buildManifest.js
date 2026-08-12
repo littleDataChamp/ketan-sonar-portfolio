8eba67cee4020f4b24d1a83166721ab187aa71f6
Ketan Sonar <ketansonar0708@gmail.com>
2026-08-12 12:19:02 +0530

    Updates

--- _next/static/etpO3jbPe4pmYcrFi1ll6/_buildManifest.js
+++ _next/static/etpO3jbPe4pmYcrFi1ll6/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/ketan-sonar-portfolio//_next/:path+",
-        "destination": "/ketan-sonar-portfolio/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
